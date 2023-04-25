96c8456894db489282bc6bddbfe52f969e43507f
devUserContact <developer.user.contact@protonmail.com>
2023-04-25 10:43:57 -0700

    added minimist package to implement command line arguments

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 require('dotenv').config()
+const apiKey = process.env.POLYGONSCAN_API_KEY
+const symbolArg = require('minimist')(process.argv.slice(2))
+const symbol = symbolArg._[0]
 const tokenList = require('./tokenList/quickswap-default.tokenlist.json')
-const apiKey = process.env.POLYGONSCAN_API_KEY;
+
+getTokenBySymbol(symbol, tokenList)
 
 function getTokenBySymbol(symbol, tokenList) {
   let requestedTokens = []
@@ -15,7 +19,9 @@ function getTokenBySymbol(symbol, tokenList) {
       `https://api.polygonscan.com/api?module=stats&action=tokensupply&contractaddress=${token.address}&apikey=${apiKey}`
     )
       .then((res) => res.json())
-      .then((data) => {if (data.result != 0) console.table(data.result)})
-  });
+      .then((data) => {
+        if (data.result != 0)
+          console.log(`${symbol} has total supply of: ${data.result}`)
+      })
+  })
 }
-getTokenBySymbol('DAI', tokenList)
